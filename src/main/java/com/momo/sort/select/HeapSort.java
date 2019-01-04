da8737da58e641080e04c4a9cabc402b9fd36db1
MQG <mo1056610188@163.com>
2019-01-04 10:28:57 +0800

    堆排序

--- src/main/java/com/momo/sort/select/HeapSort.java
+++ src/main/java/com/momo/sort/select/HeapSort.java
@@ -11,37 +11,42 @@ public class HeapSort {
 
     public static void sort(int nums[]) {
         // 构造最小堆
-        for (int i = nums.length / 2 - 1; i >= 0; i--) {
-            sift(nums, i, nums.length - 1);
-        }
+        buildHeap(nums);
+
         // 升序排序，每趟确定1个最小值，即堆顶值
         for (int i = nums.length - 1; i > 0; i--) {
             swap(nums, 0, i);
-            sift(nums, 0, i - 1);
+            sift(nums, 0, i);
         }
     }
 
-    private static void sift(int[] nums, int parent, int end) {
-        int child = 2 * parent + 1;
+    private static void buildHeap(int nums[]) {
+        for (int i = nums.length / 2 - 1; i >= 0; i--) {
+            sift(nums, i, nums.length);
+        }
+    }
 
-        while (child <= end) {
+    private static void sift(int[] nums, int parent, int length) {
+        int child = 2 * parent + 1;
+        int value = nums[parent];
+
+        while (child < length) {
             // 最小的子节点
-            if (child < end && nums[child] > nums[child + 1]) {
+            if (child + 1 < length && nums[child] > nums[child + 1]) {
                 child++;
             }
-            // 子节点小于父节点，交换；同时parent、child下沉
-            if (nums[parent] > nums[child]) {
-                // 交换
-                int temp = nums[parent];
-                nums[parent] = nums[child];
-                nums[child] = temp;
-                // 下沉
-                parent = child;
-                child = 2 * parent + 1;
-            } else {
+
+            // 如果父节点小于任何一个孩子的值，直接跳出
+            if (nums[parent] <= nums[child]) {
                 break;
             }
+            // 子节点小于父节点，交换(无需真正交换)；同时parent、child下沉
+            nums[parent] = nums[child];
+            // 下沉
+            parent = child;
+            child = 2 * parent + 1;
         }
+        nums[parent] = value;
     }
 
     private static void swap(int[] nums, int i, int j) {
